f2f13e7f992228524b62d4069cca68d63206c39b
Faiyaz Rafeek <fayaznahwi@gmail.com>
2020-04-23 00:11:01 +0530

    simple crypto currency application

---
+++ angular-crypto/src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  result:any;
+
+  constructor(private _http: HttpClient) { }
+
+  getPrices() {
+    return this._http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD")
+    .pipe(map(result => this.result = result));
+  }
+}
